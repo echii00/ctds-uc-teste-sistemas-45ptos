d412350f58f8764f8d555e083283748b75ef01c6
Échilley <echilleypereirasilva@gmail.com>
2025-07-31 21:29:44 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import CadastroGeral from './components/cadastroGeral'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <CadastroGeral/>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/cadastroGeral.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+const CadastroGeral = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (nome || email.includes('@') || senha.length < 6) {
+      setMensagem(' ');
+      return;
+    }
+
+    setMensagem('Cadastro realizado com sucesso!');
+    setNome('');
+    setEmail('');
+    setSenha('');
+  };
+
+  return (
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+      <h2>Cadastro de Usuário</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nome:</label>
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="nome "
+            
+          />
+        </div>
+
+        <div>
+          <label>E-mail:</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email "
+            
+          />
+        </div>
+
+        <div>
+          <label>Senha:</label>
+          <input
+            type="number" 
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="senha"
+            
+          />
+        </div>
+
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      {mensagem || <p>{mensagem}</p>}
+    </div>
+  );
+};
+
+export default CadastroGeral;
